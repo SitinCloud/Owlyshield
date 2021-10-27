f8f90f3bb71680dbf7c4bc7dc5a82eaf266da4c0
Damien LESCOS <damien.lescos@sitincloud.com>
2021-10-27 10:07:02 +0200

    [learn] feat: learn on data written by predict
    preprocess() can now work on learn.csv as created by owlyshield_predict.

--- owlyshield_learning/main.py
+++ owlyshield_learning/main.py
@@ -1,4 +1,5 @@
 import datetime
+import json
 import joblib
 import numpy as np
 import pandas as pd
@@ -10,47 +11,92 @@ from tensorflow import keras
 from tensorflow.python.keras import Sequential
 from tensorflow.python.keras.layers import Dense, Dropout, LSTM
 
-ROWS_LEN = 20
+ROWS_LEN = 50
 COLS_LEN = 21
 
-def load_inputs(df, is_test_set, min_set_len):
-    # df_tmp = df[df['test_set'] == is_test_set]
-    df_tmp = df
-    X = []
-    Y = []
-    sample_ids = df_tmp['sample_id'].unique()
-    for sample_id in sample_ids:
-        df_sample_id = df_tmp[df_tmp['sample_id'] == sample_id]
-        sample_id_len = len(df_sample_id)
-        if sample_id_len < min_set_len:
-            continue
-        X_temp = []
-        idx_zero = 0
-        for (idx, row) in df_sample_id.iterrows():
-            if (idx_zero + min_set_len) >= sample_id_len:
-                break
-            if len(X_temp) == min_set_len:
-                X.append(X_temp)
-                Y.append(row['is_ransom'])
-                X_temp = []
-            X_temp.append(row.tolist()[3:24])
-            idx_zero += 1
+def preprocess(from_path, length):
+    columns = ['app_name', 'gid', 'sum_entropy_weight_r', 'sum_entropy_weight_w', 'extensions_count_r',
+               'extensions_count_w',
+               'file_ids_c_count', 'file_ids_d_count', 'file_ids_r_count', 'file_ids_rn_count', 'file_ids_w_count',
+               'file_ids_u_count',
+               'extensions_count_u', 'files_paths_u_count', 'pids_count', 'extensions_count_w_doc',
+               'extensions_count_w_archives',
+               'extensions_count_w_db', 'extensions_count_w_code', 'extensions_count_w_exe',
+               'dir_with_files_c_count', 'dir_with_files_u_count', 'exe_exists']
 
-    return X, Y
+    df = pd.read_csv(from_path, names=columns, sep=';')
+
+    df['is_ransom'] = df['app_name'].astype(str).apply(lambda x: 'Virus' in x)
+
+    ransom_samples_cnt = df[df['is_ransom']].shape[0]
+    legit_samples_cnt = df[df['is_ransom'] == False].shape[0]
+    print(f'{ransom_samples_cnt} ransom samples')
+    print(f'{legit_samples_cnt} legit samples')
+
+    dataX = []
+    dataY = []
+    x_temp = []
+    y_temp = False
+
+    old_gid = -1
+    for (idx, row) in df.iterrows():
+        if row['gid'] != old_gid and old_gid != -1:
+            dataX.append(x_temp)
+            dataY.append([y_temp])
+            x_temp = []
+        x_temp.append(row.tolist()[2:-1])
+        y_temp = row['is_ransom']
+        old_gid = row['gid']
+
+    for i in list(range(len(dataX))):
+        dataX[i] = seq_diff(dataX[i])
+
+    x, y = remove_short(dataX, dataY, length, 200)
+
+    return x, y
+
+
+def remove_short(dataX, dataY, length, seq_count_max):
+    new_X = []
+    new_Y = []
+
+    max_idx = length * seq_count_max + 1
+    idx = 0
+    while idx + length < max_idx:
+        for x in list(range(len(dataX))):
+            if len(dataX[x]) >= idx + length:
+                new_X.append(dataX[x][idx:(idx + length)])
+                new_Y.append(dataY[x][:length])
+        idx += length
+
+    x, y = to_numpy_tensors(new_X, new_Y)
+    return x, y
+
+
+def to_numpy_tensors(x, y):
+    x_shape = (len(x), len(x[0]), len(x[0][0]))
+    y_shape = (len(y), 1)
+
+    if type(x) is list:
+        x = np.array(x)
+    if type(y) is list:
+        y = np.array(y)
+
+    x = x.reshape(x_shape)
+    y = y.reshape(y_shape)
+
+    return x, y
 
 
 def train_model(x_train, y_train, x_val, y_val):
     model = Sequential()
-    model.add((LSTM(units=80, return_sequences=True, input_shape=(x_train.shape[1], COLS_LEN))))
+    model.add(LSTM(units=80, activation='tanh', return_sequences=True, input_shape=(ROWS_LEN, COLS_LEN)))
     model.add(Dropout(0.2))
 
-    model.add((LSTM(units=80, return_sequences=True)))
+    model.add(LSTM(units=80, activation='tanh', return_sequences=True))
     model.add(Dropout(0.2))
 
-    # model.add((LSTM(units=80, return_sequences=True)))
-    # model.add(Dropout(0.2))
-
-    model.add(LSTM(units=50))
+    model.add(LSTM(units=80, activation='tanh'))
     model.add(Dropout(0.2))
 
     model.add(Dense(1, activation='sigmoid'))
@@ -59,15 +105,15 @@ def train_model(x_train, y_train, x_val, y_val):
                   optimizer='adam',
                   metrics=['accuracy'])
 
-    model.summary()
     log_dir = "logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
     tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
 
-    model.fit(x_train, y_train, batch_size=32, epochs=25, verbose=1,
+    model.fit(x_train, y_train, batch_size=32, epochs=20, verbose=1,
               validation_data=(x_val, y_val),
               shuffle=False,
               callbacks=[tensorboard_callback])  # for testing
 
+    # model.summary()
     model.save('./models/model_lstm.h5')
     eval_model(model, x_val, y_val)
 
@@ -79,54 +125,65 @@ def eval_model(model, x_val, y_val):
     print(classification_report(y_val, y_pred_bool))
 
 
+def seq_diff(l):
+    """
+    [ [0, 1], [0, 2], [1, 3] ] -> [ [0, 1], [1, 1] ]
+    """
+    return l
+    # res = []
+    # for i in range(len(l) - 1):
+    #     value = l[i:i + 2]
+    #     l_l = np.array(value[0])
+    #     l_r = np.array(value[1])
+    #     res.append((l_r - l_l).tolist())
+    # return res
+
+
 def load_data():
     pd.set_option('display.max_columns', None)
-    sep = ","
-    path = r".\data\learn_data_small_example.csv"
-    # path = r'D:\Azure\Learn270721\learn_all_f.csv';
-    df = pd.read_csv(path, sep=sep)
-    # df = pd.read_csv(path, sep=sep, nrows=10000)
+    path = r'.\data\learn_data_small_example.csv'
 
-    x, y = load_inputs(df, False, 10)
+    x, y = preprocess(path, ROWS_LEN)
 
-    x_train, x_val, y_train, y_val = train_test_split(x, y, test_size=0.25, random_state=42, shuffle=True)
+    x_train, x_val, y_train, y_val = train_test_split(x, y, test_size=0.20, random_state=42, shuffle=True)
 
-    xx_train = np.array(x_train)
-    xx_val = np.array(x_val)
-    yy_train = np.array(y_train)
-    yy_val = np.array(y_val)
+    x_train = np.array(x_train)
+    x_val = np.array(x_val)
+    y_train = np.array(y_train)
+    y_val = np.array(y_val)
 
     scaler = StandardScaler()
-    xx_train = scaler.fit_transform(xx_train.reshape(-1, xx_train.shape[-1])).reshape(xx_train.shape)
-    xx_val = scaler.transform(xx_val.reshape(-1, xx_val.shape[-1])).reshape(xx_val.shape)
+    x_train = scaler.fit_transform(x_train.reshape((-1, COLS_LEN))).reshape((-1, ROWS_LEN, COLS_LEN))
+    x_val = scaler.transform(x_val.reshape((-1, COLS_LEN))).reshape((-1, ROWS_LEN, COLS_LEN))
+
     joblib.dump(scaler, './models/scaler_lstm.save')
 
-    return (xx_train, yy_train, xx_val, yy_val)
+    return x_train, y_train, x_val, y_val
 
 
-def convert_model_to_tflite(model):
+def convert_model_to_tflite():
+    model = keras.models.load_model('./models/model_lstm.h5')
     converter = tf.lite.TFLiteConverter.from_keras_model(model)
     tflite_model = converter.convert()
-    with open(r'.\models\\converted.tflite', 'wb') as f:
+    with open(r'.\models\\model.tflite', 'wb') as f:
         f.write(tflite_model)
+    save_scaler_json()
 
 
-def Normalize(data, mean_data=None, std_data=None):
-    if not mean_data:
-        mean_data = np.mean(data)
-    if not std_data:
-        std_data = np.std(data)
-    norm_data = (data - mean_data) / std_data
-    return norm_data, mean_data, std_data
+def save_scaler_json():
+    scaler = joblib.load('./models/scaler_lstm.save')
+    with open('./models/mean.json', 'w') as fp:
+        json.dump(scaler.mean_.tolist(), fp)
+    with open('./models/std.json', 'w') as fp:
+        json.dump(scaler.var_.tolist(), fp)
 
 
-def main_lstm():
+def main():
     x_train, y_train, x_val, y_val = load_data()
     train_model(x_train, y_train, x_val, y_val)
-    model = keras.models.load_model('./models/model_lstm.h5')
-    scaler = joblib.load('./models/scaler_lstm.save')
+    # model = keras.models.load_model('./models/model_lstm.h5')
     # eval_model(model, x_val, y_val)
-    # convert_model_to_tflite(model)
 
 
-main_lstm()
+main()
+convert_model_to_tflite()
