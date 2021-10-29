1b7a5bc72a70222b450c8dad0764f0b6e2c988a2
aoyhenart <92170092+aoyhenart@users.noreply.github.com>
2021-10-29 10:58:42 +0200

    [rust_win_toast] Add arguments and possibility to launch a log file
    This commit improves the use of toast notifications.
    We can now use a logo.
    Notifications can also be marked as coming from Owlyshield and not from Powershell.
    We can also open a log file when the notification is sent.
    
    To make full use of these features, we recommend running the latest version of the Innosetup installation script.

--- rust_win_toast/src/main.rs
+++ rust_win_toast/src/main.rs
@@ -1,21 +1,48 @@
-use winrt_notification::{Toast, Sound, Duration};
+use winrt_notification::{Toast, Sound, Duration, IconCrop};
 use std::env;
+use std::path::Path;
+use std::process::Command;
 
 fn main() {
     for argument in env::args() {
         println!("arg {}", argument);
     }
     let arguments: Vec<String> = env::args().collect();
-    if env::args().len() > 2 {
-    Toast::new(Toast::POWERSHELL_APP_ID)
-        .title(&arguments[1])
-        .text1(&arguments[2])
-        .sound(Some(Sound::SMS))
-        .duration(Duration::Short)
-        .show()
-        .expect("unable to toast");
+    if env::args().len() == 3 {
+        Toast::new(Toast::POWERSHELL_APP_ID)
+            .title(&arguments[1])
+            .text1(&arguments[2])
+            .sound(Some(Sound::SMS))
+            .duration(Duration::Short)
+            .show()
+            .expect("unable to toast");
+    } else if env::args().len() > 3 {
+        let logo = if env::args().len() > 4 { &arguments[3] } else { "" };
+        let app = if env::args().len() > 5 { &arguments[4] } else { Toast::POWERSHELL_APP_ID };
+        let log = &arguments[env::args().len()-1];
+        Toast::new(app)
+            .title(&arguments[1])
+            .text1(&arguments[2])
+            .icon(Path::new(logo), IconCrop::Square, "")
+            .sound(Some(Sound::SMS))
+            .duration(Duration::Short)
+            .show()
+            .expect("unable to toast");
+        if log.ends_with(".html") || log.ends_with(".txt") || log.ends_with(".log") {
+            if cfg!(target_os = "windows") {
+                Command::new("cmd")
+                    .arg("/C").arg(log)
+                    .output()
+                    .expect("failed to execute process")
+            } else {
+                Command::new("sh")
+                    .arg("-c")
+                    .arg(log)
+                    .output()
+                    .expect("failed to execute process")
+            };
+        }
     } else {
         println!("bad number of args");
     }
-
 }
