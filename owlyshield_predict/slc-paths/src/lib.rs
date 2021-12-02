7bdef9f77862ed714e1bdd1bfe43f4a2d181ac11
Allande OYHENART <allande.oyhenart@sitincloud.com>
2021-12-02 16:14:28 +0100

    doc: slc-path documentation

--- owlyshield_predict/slc-paths/src/lib.rs
+++ owlyshield_predict/slc-paths/src/lib.rs
@@ -1,7 +1,74 @@
+//! [slc-path] crate is based on the [`single-linkage_clustering`](https://en.wikipedia.org/wiki/Single-linkage_clustering) method.
+//!
+//! In statistics, single-linkage clustering is one of several methods of hierarchical clustering.
+//! It is based on grouping clusters in bottom-up fashion (agglomerative clustering), at each step combining two clusters that contain the closest pair of elements not yet belonging to the same cluster as each other.
+//!
+//! This module principally use the [`kodama`](https://docs.rs/kodama/latest/kodama/) crate.
+//!
+//! Clustering allows to trace the extent of the impact of a program on the file tree.
+
+// # Example
+// ## First step
+// ### First clustering
+// Let us assume that we have five elements (a,b,c,d,e) and the following matrix D1 of pairwise distances between them:
+//
+// |          | a  | b  | c  | d  | e   |
+// |----------|----|----|----|----|-----|
+// | <b>a</b> | 0  | 17 | 21 | 31 | 23  |
+// | <b>b</b> | 17 | 0  | 30 | 34 | 21  |
+// | <b>c</b> | 21 | 30 | 0  | 28 | 39  |
+// | <b>d</b> | 31 | 34 | 28 | 0  | 43  |
+// | <b>e</b> | 23 | 21 | 39 | 43 | 0   |
+//
+// In this example, D1(a,b)=17 is the lowest value of D1, so we cluster elements a and b.
+//
+// ### First distance matrix update
+// We then proceed to update the initial proximity matrix D1 into a new proximity matrix D2 (see below), reduced in size by one row and one column because of the clustering of a with b.
+// Bold values in D2 correspond to the new distances, calculated by retaining the minimum distance between each element of the first cluster (a,b) and each of the remaining elements:
+//
+// D2((a,b),c)=min(D1(a,c),D1(b,c))=min(21,30)=21
+//
+// D2((a,b),d)=min(D1(a,d),D1(b,d))=min(31,34)=31
+//
+// D2((a,b),e)=min(D1(a,e),D1(b,e))=min(23,21)=21
+//
+// Italicized values in D2 are not affected by the matrix update as they correspond to distances between elements not involved in the first cluster.
+//
+// ## Second step
+// ### Second clustering
+// We now reiterate the three previous actions, starting from the new distance matrix D2 :
+//
+// |              | (a,b)     | c         | d         | e          |
+// |--------------|-----------|-----------|-----------|------------|
+// | <b>(a,b)</b> | 0         | <b>21</b> | <b>31</b> | <b>21</b>  |
+// | <b>c</b>     | <b>21</b> | 0         | 28        | 39         |
+// | <b>d</b>     | <b>31</b> | 28        | 0         | 43         |
+// | <b>e</b>     | <b>21</b> | 39        | 43        | 0          |
+//
+// Here, D2((a,b),c)=21 and D2((a,b),e)=21 are the lowest values of D2, so we join cluster (a,b) with element c and with element e.
+//
+// ### Second distance matrix update
+// We then proceed to update the D2 matrix into a new distance matrix D3 (see below), reduced in size by two rows and two columns because of the clustering of (a,b) with c and with e :
+//
+// D3(((a,b),c,e),d)=min(D2((a,b),d),D2(c,d),D2(e,d))=min(31,28,43)=28
+//
+// ## Final step
+// The final D3 matrix is:
+//
+// |                    | ((a,b),c,e) | d         |
+// |--------------------|-------------|-----------|
+// | <b>((a,b),c,e)</b> | 0           | <b>28</b> |
+// | <b>d</b>           | <b>28</b>   | 0         |
+//
+// So we join clusters ((a,b),c,e) and d.
+//
+// Let r denote the (root) node to which ((a,b),c,e) and d are now connected.
+
+/// This module manages clusters generated from filepath lists.
 pub mod clustering {
+    use kodama::{Method, linkage};
     use std::path::Path;
     use std::cmp;
-    use kodama::{Method, linkage};
     use std::fs::File;
     use std::io::BufReader;
     use std::io::BufRead;
@@ -14,19 +81,22 @@ pub mod clustering {
     }
 
     impl Cluster {
+        /// Returns the common root of the cluster files.
         pub fn root(&self) -> String {
             self.root.clone()
         }
-
+        /// Returns the number of files in the cluster.
         pub fn size(&self) -> usize {
             self.size
         }
-
+        /// Returns the step where the cluster was created.
         pub fn step(&self) -> usize {
             self.step
         }
     }
 
+    /// Returns the list of [Cluster] from a file containing a list of filepaths.
+    /// This function calls the [clustering] function.
     pub fn clustering_from_file(filename: &str) -> Vec<Cluster> {
         let mut strpaths: HashSet<String> = HashSet::new();
         let file = File::open(filename).unwrap();
@@ -38,6 +108,13 @@ pub mod clustering {
         return clustering(strpaths);
     }
 
+    /// Returns the list of [Cluster] from a list of filepaths.
+    /// It builds a dendogram from the input list and analyzes it to return a list of clusters.
+    /// The returned list is the list of clusters just before the moment where the dissimilarity between 2 successive steps is the greatest.
+    ///
+    /// There are 2 cases where the behavior is different :
+    /// If the input list is empty, it will return an empty list
+    /// If the input list contains less than 3 elements, it will return as many clusters as there are filepaths in input
     pub fn clustering(strpaths: HashSet<String>) -> Vec<Cluster> {
         if strpaths.len() == 0 {
             return vec![];
@@ -161,6 +238,7 @@ pub mod clustering {
         return clusters;
     }
 
+    /// Returns the distance between 2 files in the file tree.
     pub fn distance(x: &Path, y: &Path) -> f32 {
         if x.as_os_str() == y.as_os_str() {
             return 0.0; //Même fichier
@@ -204,6 +282,7 @@ pub mod clustering {
 }
 
 #[cfg(test)]
+#[doc(hidden)]
 mod tests {
     // use crate::clustering::clustering;
     use crate::clustering::clustering_from_file;
