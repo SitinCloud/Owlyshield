d1296f8a292923c2c9fd6b8499e08f4a6a588b94
sn99 <siddharthn.099@gmail.com>
2023-01-10 11:46:18 +0530

    fix issue of floating point on 32-bit systems

--- owlyshield_minifilter/OwlyshieldRansomFilter/ShanonEntropy.cpp
+++ owlyshield_minifilter/OwlyshieldRansomFilter/ShanonEntropy.cpp
@@ -1,14 +1,13 @@
-
 #include "ShanonEntropy.h"
 
 constexpr DOUBLE M_LOG2E = 1.4426950408889634;
 
 constexpr ULONG MAX_BYTE_SIZE = 256;
 
-DOUBLE shannonEntropy(PUCHAR buffer, size_t size)
+_Kernel_float_used_ DOUBLE shannonEntropy(PUCHAR buffer, size_t size)
 {
     if (IS_DEBUG_IRP)
-        DbgPrint("!!! FSFilter: Calc entropy started\n");
+        DbgPrint("!!! snFilter: Calc entropy started\n");
     DOUBLE entropy = 0.0;
     ULONG bucketByteVals[MAX_BYTE_SIZE] = {};
     for (ULONG i = 0; i < size; i++)
@@ -16,22 +15,21 @@ DOUBLE shannonEntropy(PUCHAR buffer, size_t size)
         bucketByteVals[buffer[i]]++;
     }
 
-    XSTATE_SAVE SaveState;
     __try
     {
-        KeSaveExtendedProcessorState(XSTATE_MASK_LEGACY, &SaveState);
         for (ULONG i = 0; i < MAX_BYTE_SIZE; i++)
         {
             if (bucketByteVals[i] != 0)
             {
-                DOUBLE val = (DOUBLE)bucketByteVals[i] / (DOUBLE)size;
+                DOUBLE
+                val = (DOUBLE)bucketByteVals[i] / (DOUBLE)size;
                 entropy += (-1) * val * log(val) * M_LOG2E;
             }
         }
     }
     __finally
     {
-        KeRestoreExtendedProcessorState(&SaveState);
     }
+
     return entropy;
 }
